c1f7eb31f5373d934108fbc8a728bec432b41877
FarzadShamriz <farzadshamriz@outlook.com>
2019-03-06 03:24:04 -0500

    Add Main to related folder

---
+++ src/conquestgame/MainConquest.java
@@ -0,0 +1,16 @@
+package conquestgame;
+import Manager.ConquestUI;
+import Model.*;
+
+public class MainConquest {
+
+	public static void main(String[] args) 
+	{
+		Editor editor = new Editor();
+		Map map = new Map();
+		editor.setCountries(map.CountriesList);
+		ConquestUI uiManager = new ConquestUI();
+		uiManager.conquestUIShowStartMenu();
+	}
+
+}
