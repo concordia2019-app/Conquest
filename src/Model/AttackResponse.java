e6c0d1c79b0f75c54bc6d10156be8b444be26ced
FarzadShamriz <farzadshamriz@outlook.com>
2019-03-11 15:27:48 -0400

    Create a class for returning attack result to UI.

---
+++ src/Model/AttackResponse.java
@@ -0,0 +1,17 @@
+package Model;
+
+/**
+ * This class is using for returning attack status
+ * 
+ * @author farzad
+ *
+ */
+public class AttackResponse {
+	
+	// attackStatus is a boolean property that return true (when attacker is wining) and false (when attacker is loosing)
+	private boolean attackStatus;
+	
+	//restOfArmies is an integer property to return the rest of armies of attack
+	private int restOfArmies;
+	
+}
