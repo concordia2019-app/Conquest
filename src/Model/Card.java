65149da82e886078d17cc01b8ced281205ee3fba
Ali Hasheminezhad <ahasheminezhad@yahoo.com>
2019-03-15 12:12:54 -0400

    Added Card class constuctor and fields.

--- src/Model/Card.java
+++ src/Model/Card.java
@@ -1,5 +1,34 @@
 package Model;
 
+/**
+ * This class is the model for Card.
+ * 
+ * @author AHasheminezhad
+ */
 public class Card {
+	
+	private enum cardTypes { INFANTRY, CAVALRY, ARTILLERY };
+	
+	private cardTypes cardType;
+	
+	public Card(int cardTypeIndex) {
+		switch (cardTypeIndex) {
+		case 1:
+			this.cardType = cardTypes.INFANTRY;
+			break;
+		case 2:
+			this.cardType = cardTypes.CAVALRY;
+			break;
+		case 3:
+			this.cardType = cardTypes.ARTILLERY;
+			break;
 
+		default:
+			System.out.println("Input index for typeCard is wrong.");
+		}
+	}
+	
+	public cardTypes getCardType() {
+		return cardType;
+	}
 }
