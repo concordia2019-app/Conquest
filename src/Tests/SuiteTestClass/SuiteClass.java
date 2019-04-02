34465d984d6df82c622f46953ed9cb1c5271b387
FarzadShamriz <farzadshamriz@outlook.com>
2019-04-02 15:41:57 -0400

    Add SuiteClass to project to run all unit test together

---
+++ src/Tests/SuiteTestClass/SuiteClass.java
@@ -0,0 +1,23 @@
+package Tests.SuiteTestClass;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import Controller.ConquestController;
+import Tests.Controller.ConquestControllerTest;
+import Tests.Helper.CountryHelperTest;
+import Tests.Helper.PlayerHelperTest;
+import Tests.Helper.UIHelperTest;
+import Tests.Model.CardTest;
+import Tests.Model.FinishGameTest;
+import Tests.Model.MapGeneratorTest;
+import Tests.Model.MapTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({ MapTest.class, ConquestControllerTest.class, CountryHelperTest.class, PlayerHelperTest.class,
+		UIHelperTest.class, CardTest.class, FinishGameTest.class, ConquestControllerTest.class,
+		MapGeneratorTest.class })
+public class SuiteClass {
+
+}
