2b39c39cfab1e6f5f3bc606a999c2bd2b2c35db2
FarzadShamriz <farzadshamriz@outlook.com>
2019-04-04 12:55:22 -0400

    Add Benovolent player class that extends from Player

--- src/Model/BenovolentPlayer.java
+++ src/Model/BenovolentPlayer.java
@@ -6,6 +6,8 @@
 package Model;
 
 import java.util.ArrayList;
+import Helper.CountryHelper;
+import Helper.PlayerHelper;
 
 public class BenovolentPlayer extends Player {
 
@@ -24,10 +26,65 @@ public class BenovolentPlayer extends Player {
 
 	@Override
 	public void movePlayer(ArrayList<Country> countryList) {
-		//TODO find the weaker country
-		
-		//TODO find the adj of weaker country 
-		
-		//TODO armies to weeker one
+		PlayerHelper playerHelper = new PlayerHelper();
+		ArrayList<Country> playerCountries = playerHelper.getPlayerCountries(countryList, this.getPlayerID());
+		Country playerWeakerCountry = playerHelper.getWeakerCountry(playerCountries);
+		Country playerSourceCountryForMoving = playerHelper.getFamilyAdjacentWithMoreThanOneArmy(playerCountries,
+				playerWeakerCountry);
+		if (playerSourceCountryForMoving != null) {
+			ArrayList<Country> updatedCountryList = moveCalculationForBenovolentPlayer(countryList,
+					playerSourceCountryForMoving, playerWeakerCountry);
+			CountryHelper countryHelper = new CountryHelper();
+			boolean updateSucceed = false;
+			while (!updateSucceed) {
+				updateSucceed = countryHelper.updateSourceCountriesArmies(updatedCountryList);
+			}
+		}
 	}
+
+	public ArrayList<Country> moveCalculationForBenovolentPlayer(ArrayList<Country> countryList,
+			Country sourcePlayerCountry, Country destinationPlayerCountry) {
+		int destinationArmies = destinationPlayerCountry.getArmy();
+		int sourceArmies = sourcePlayerCountry.getArmy();
+		int armiesToAdd = ((sourceArmies - destinationArmies) + destinationArmies);
+		int restOfArmiesForSource = destinationArmies;
+		destinationPlayerCountry.setArmy((armiesToAdd));
+		sourcePlayerCountry.setArmy(restOfArmiesForSource);
+
+		for (Country countryItem : countryList) {
+			if (countryItem.getCountryID() == sourcePlayerCountry.getCountryID())
+				countryItem.setArmy(sourcePlayerCountry.getArmy());
+			if (countryItem.getCountryID() == destinationPlayerCountry.getCountryID())
+				countryItem.setArmy(destinationPlayerCountry.getArmy());
+		}
+		return countryList;
+	}
+
+	public ArrayList<Country> reinforcementPlayer(ArrayList<Country> countryList) {
+		ArrayList<Card> playerCards = this.getCards();
+		PlayerHelper playerHelper = new PlayerHelper();
+		ArrayList<Country> playerCountries = playerHelper.getPlayerCountries(countryList, this.getPlayerID());
+		Country playerWeakerCountry = playerHelper.getWeakerCountry(playerCountries);
+
+		if (playerHelper.playerUseCardDecide(playerCards)) {
+			ArrayList<Country> updatedCountryList = new ArrayList<Country>();
+			for (Country countryItem : countryList) {
+				if (countryItem.getCountryID() == playerWeakerCountry.getCountryID()) {
+					int weakerCountryArmies = playerWeakerCountry.getArmy();
+					int armiesToAdd = (weakerCountryArmies + this.getReinforcementPlayerArmies());
+					countryItem.setArmy(armiesToAdd);
+				}
+				updatedCountryList.add(countryItem);
+			}
+
+			boolean updateSucceed = false;
+			CountryHelper countryHelper = new CountryHelper();
+			while (!updateSucceed) {
+				updateSucceed = countryHelper.updateSourceCountriesArmies(updatedCountryList);
+			}
+		}
+
+		return countryList;
+	}
+
 }
