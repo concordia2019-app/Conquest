12709e4e040728efdb0f9f49a2e0e615b5ea2590
FarzadShamriz <farzadshamriz@outlook.com>
2019-04-09 11:41:24 -0400

    Refactoring - Modify calling Tournament view

--- src/Controller/TournamentController.java
+++ src/Controller/TournamentController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 import javax.activation.MailcapCommandMap;
 
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
+
 import Model.AggressivePlayer;
 import Model.BenovolentPlayer;
 import Model.CheaterPlayer;
@@ -13,7 +15,9 @@ import Model.MapGenerator;
 import Model.Player;
 import Model.PlayerNameAndType;
 import Model.RandomPlayer;
+import Model.TournamentResult;
 import View.ConquestUI;
+import View.TournamentView;
 
 public class TournamentController {
 
@@ -35,13 +39,15 @@ public class TournamentController {
 		int numberOfMaps = conquestUI.getNumberOfMaps();
 		int numberOfGamesPerMap = conquestUI.getNumberOfGamesPerMap();
 		int numberOfTurnsPerGame = conquestUI.getNumberOfTurnsPerGame();
+		ArrayList<TournamentResult> tournamentResults = new ArrayList<TournamentResult>();
 
 		// Map Counter loop
 		for (int mapCounter = 0; mapCounter < numberOfMaps; mapCounter++) {
 			boolean readFileStatusMap = getFilePathForLoadingMap(System.getProperty("user.dir")
-					+ "\\bin\\ResourceProject\\CountryTournament" + (mapCounter + 1) + ".json");
+					+ "\\src\\TournamentMaps\\CountryTournament" + (mapCounter + 1) + ".json");
 			Map.getInstance().assigningPlayerCountries(playerNamesAndTypes);
 			Player[] players = Map.getInstance().getPlayers();
+			ConquestController conquestController = ConquestController.getInstance();
 
 			// If map loaded completely
 			if (readFileStatusMap) {
@@ -59,6 +65,9 @@ public class TournamentController {
 										playerItem.getPlayerName(), playerItem.getCountryID());
 								aggressivePlayer.aggressiveReinforcementPlayer(Map.getInstance().getCountries());
 								aggressivePlayer.aggressiveAttackPlayer();
+								if (conquestController.isGameFinish()) {
+
+								}
 								aggressivePlayer.aggressiveMovePlayer(Map.getInstance().getCountries());
 								break;
 							case BENOVOLENT:
@@ -87,20 +96,73 @@ public class TournamentController {
 							}
 						}
 					}
-
+					TournamentResult tournamentResult = new TournamentResult();
+					tournamentResult.setGameIndex(gameCounter);
+					tournamentResult.setMapIndex(mapCounter);
+					tournamentResult.setWinnerName(getWinnerName(Map.getInstance().getCountries()));
+					tournamentResults.add(tournamentResult);
 				}
 
 			}
 
 		}
-
-		Map map = Map.getInstance();
-		map.assigningPlayerCountries(playerNamesAndTypes);
+		TournamentView tournamentView = new TournamentView();
+		tournamentView.tournamentResult(Map.getInstance().getCountries(), numberOfMaps, playerNumber,
+				numberOfGamesPerMap, tournamentResults);
+		// Map map = Map.getInstance();
+		// map.assigningPlayerCountries(playerNamesAndTypes);
 
 	}
 
-	private void createAndAssignMapAndPlayers() {
+	public String getWinnerName(ArrayList<Country> countryList) {
+		ArrayList<Integer> playerScores = new ArrayList<Integer>();
+		int p1 = 0;
+		int p2 = 0;
+		int p3 = 0;
+		int p4 = 0;
+		int p5 = 0;
+		int pScoreTemp = 0;
 
+		for (Country country : countryList) {
+			switch (country.getPlayerID()) {
+			case 1:
+				pScoreTemp = p1;
+				p1 = (pScoreTemp + 1);
+				break;
+			case 2:
+				pScoreTemp = p2;
+				p2 = (pScoreTemp + 1);
+				break;
+			case 3:
+				pScoreTemp = p3;
+				p3 = (pScoreTemp + 1);
+				break;
+			case 4:
+				pScoreTemp = p4;
+				p4 = (pScoreTemp + 1);
+				break;
+			case 5:
+				pScoreTemp = p5;
+				p5 = (pScoreTemp + 1);
+				break;
+			}
+		}
+		playerScores.add(p1);
+		playerScores.add(p2);
+		playerScores.add(p3);
+		playerScores.add(p4);
+		playerScores.add(p5);
+		int winnerId = 0;
+		for (int j = 1; j < 5; j++) {
+			if (playerScores.get(winnerId) < playerScores.get(j)) {
+				winnerId = j;
+			}
+		}
+		for (Country country : countryList) {
+			if (country.getPlayerID() == (winnerId + 1))
+				return country.getPlayerName();
+		}
+		return "-";
 	}
 
 	private boolean getFilePathForLoadingMap(String filePath) {
